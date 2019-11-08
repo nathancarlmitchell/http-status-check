1c70bdbd584739a6ff1d85890600e78ca4f1b9c4
nathancarlmitchell <nat8754@hotmail.com>
2019-11-08 13:35:20 -0500

    Update script
    Add additional HTTP codes

--- CheckHttpStatus.ps1
+++ CheckHttpStatus.ps1
@@ -12,7 +12,6 @@ foreach($url in [System.IO.File]::ReadLines($path+'http.txt')) {
         $request = Invoke-WebRequest $url -MaximumRedirection 0 -ErrorAction Ignore -TimeoutSec 1
 
         $result = $url + ',' + $request.StatusCode + ',' + $request.StatusDescription + ',' + $request.BaseResponse.ProtocolVersion.Major + '.' + $request.BaseResponse.ProtocolVersion.Minor + ',' + $request.BaseResponse.Server + ',' +$request.Headers.Location
-        #$curl.Headers.Location
 
     } catch {
 
@@ -20,11 +19,11 @@ foreach($url in [System.IO.File]::ReadLines($path+'http.txt')) {
 
             $result = $url + ',401,' + ($_ -split '\n')[0]
 
-        } elseif ((($_ -split '\n')[0]).Contains("Forbidden")) {
-
+        } elseif ((($_ -split '\n')[0]).Contains("Forbidden") -or (($_ -split '\n')[0]).Contains("You do not have permission")) {
+          # -or (($_ -split '\n')[1]).Contains("403")
             $result = $url + ',403,' + ($_ -split '\n')[0]
 
-        } elseif (($_ -split '\n')[4] -eq "HTTP Error 404. The requested resource is not found.") {
+        } elseif ((($_ -split '\n')[0]).Contains("404") -or (($_ -split '\n')[0]).Contains("Not Found") -or (($_ -split '\n')[0]).Contains("not found") -or (($_ -split '\n')[0]).Contains("could be found") -or (($_ -split '\n')[0]).Contains("The resource you are looking for has been removed")) {
 
             $result = $url + ',404,' + ($_ -split '\n')[4]
 
@@ -36,15 +35,19 @@ foreach($url in [System.IO.File]::ReadLines($path+'http.txt')) {
         
             $result = $url + ',500,' + ($_ -split '\n')[0] 
 
+        } elseif ((($_ -split '\n')[0]).Contains("Service Unavailable")) {
+        
+            $result = $url + ',503,' + ($_ -split '\n')[0] 
+
         } else {
 
             $result = $url + ',else,' + ($_ -split '\n')[0]
 
         }
-            # try { $curl = curl web1.ky.gov -MaximumRedirection 0 -ErrorAction Ignore } catch { ($_ -split '\n')[1] -eq "Runtime Error ") }
+             #try { $curl = curl kentuckyp20.ky.gov -MaximumRedirection 0 -ErrorAction Ignore } catch { ($_ ) }
     }
 
-    Add-Content -Path $path'result.csv' -Value (($result).Replace("`n","")).Replace("`n","")
+    Add-Content -Path $path'result.csv' -Value $result
 
     $count++
     $count 
